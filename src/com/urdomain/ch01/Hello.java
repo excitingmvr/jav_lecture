0d20d20f521918cbca40db06f9c2c0ea0d6a851b
tony2 <tony2@DESKTOP-PAIBPI0>
2020-12-28 00:30:12 +0900

    first

---
+++ src/com/urdomain/ch01/Hello.java
@@ -0,0 +1,19 @@
+package com.urdomain.ch01;
+
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("Hello, welcome to the java world!");
+		
+		
+		int intValue = 103029779;
+		byte byteValue = (byte) intValue;
+		System.out.println(byteValue);
+		
+		float a = 1.1f;
+		double cc = 1.1;
+		
+		float c = a + (float) cc;
+		float c = a + (float) cc;
+		double d = a + cc;
+	}
+}
